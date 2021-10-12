d2119f4dab1fc564708f150318fd431e03389a6e
Sergey Filimonov <feelqwertyg00d@gmail.com>
2021-10-12 19:22:42 +0300

    week # 3 slider

---
+++ js/overlay.js
@@ -0,0 +1,19 @@
+let hamburger  = document.querySelector('.hamburger');
+let overlay = document.querySelector('.overlay');
+let body = document.querySelector('body');
+
+let links = document.querySelectorAll('.menu__link');
+
+links.forEach(function(element){
+  element.addEventListener('click' , toggleMenu);
+})
+
+function toggleMenu(){
+  hamburger.classList.toggle('hamburger--active');
+  overlay.classList.toggle('overlay--active');
+  body.classList.toggle('body--active-menu');
+}
+
+hamburger.addEventListener('click' , toggleMenu);
+
+
