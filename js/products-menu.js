c5aebf83b99219ac3c6463c73b8575b333cfe3af
Sergey Filimonov <feelqwertyg00d@gmail.com>
2021-10-27 23:43:21 +0300

    week # 5 ops and horizontal menu

--- js/products-menu.js
+++ js/products-menu.js
@@ -1,32 +1,46 @@
 const mesureWidth = (item) => {
+  // debugger;
+  let reqItemWidth = 0; 
+
   const screenWidth = $(window).width();
   const container = item.closest(".products-menu");
   const titlesBlocks = container.find(".products-menu__title");
   const titlesWidth = titlesBlocks.width() * titlesBlocks.length;
+
+  const textContainer = item.find(".products-menu__container");
+  const paddingLeft = parseInt(textContainer.css("padding-left"));
+  const paddingRight = parseInt(textContainer.css("padding-right"));
   
   const isMobile = window.matchMedia("(max-width: 768px)").matches;
   
   if (isMobile) {
-    return screenWidth - titlesWidth;
+    reqItemWidth = screenWidth - titlesWidth;
   } else {
-    return 500;
+    reqItemWidth = 500;
+  }
+
+  return {
+    container: reqItemWidth,
+    textContainer: reqItemWidth - paddingRight - paddingLeft
   }
 };
 
-const closeEveryItemInContainer = container => {
-  const item = container.find(".products-menu__item");
+const closeEveryItemInContainer = (container) => {
+  const items = container.find(".products-menu__item");
   const content = container.find(".products-menu__content");
   
-  item.removeClass(".products-menu__line-active");
-  content.with(0);
+  items.removeClass("products-active");
+  content.width(0);
 };
 
 const openItem = (item) => {
   const hiddenContent = item.find(".products-menu__content");
   const reqWidth = mesureWidth(item);
+  const textBlock = item.find(".products-menu__container");
   
-  item.addClass(".products-menu__line-active");
-  hiddenContent.width(reqWidth);
+  item.addClass("products-active");
+  hiddenContent.width(reqWidth.container);
+  textBlock.width(reqWidth.textContainer);
 };
 
 $(".products-menu__title").on("click", (e) => {
@@ -34,7 +48,7 @@ $(".products-menu__title").on("click", (e) => {
 
   const $this = $(e.currentTarget);
   const item = $this.closest(".products-menu__item");
-  const itemOpened = item.hasClass(".products-menu__line-active");
+  const itemOpened = item.hasClass("products-active");
   const container = $this.closest(".products-menu");
   
   if (itemOpened) {
@@ -45,19 +59,9 @@ $(".products-menu__title").on("click", (e) => {
   }
 });
 
-$(".products-menu__item").on("click", e => {
-  e.preventDefault();
+// $(".products-menu__item").on("click", e => {
+//   debugger;
+//   e.preventDefault();
   
-  closeEveryItemInContainer($('.products-menu'))
-});
-
-// debugger;
-
-
-// const list = $('.products-menu');
-// list.on('click', '.products-menu__item', function (e) {
-  //   e.preventDefault()
-  //   if (e.target.classList.contains('products-menu__content')) return
-  //   $(this).siblings('li').removeClass('products-menu__line-active')
-  //   $(this).toggleClass('products-menu__line-active')
-  // });
+//   closeEveryItemInContainer($('.products-menu'))
+// });
