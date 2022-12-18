a722f3bdea3247ebeb8dad31d746329a68ab5855
Sergey Filimonov <srfilimonoff@gmail.com>
2022-12-18 22:59:36 +0300

    install gulp

---
+++ gulp.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  SRC_PATH: "src",
+  DIST_PATH: "dist",
+  STYLE_LIBS: [
+    "node_modules/normalize.css/normalize.css",
+    "node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css"
+  ],
+  JS_LIBS: [
+    "node_modules/jquery/dist/jquery.js",
+    "src/libs/bxslider/dist/jquery.bxslider.min.js",
+    "src/libs/fancybox-master/dist/jquery.fancybox.min.js",
+    "node_modules/mobile-detect/mobile-detect.js",
+    "node_modules/jquery-touchswipe/jquery.touchSwipe.js",
+  ]
+}
---
+++ gulpfiles.js
@@ -0,0 +1,16 @@
+const { src, dest, task, series, watch, parallel } = require('gulp');
+const rm = require('gulp-rm');
+const sass = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
+const sassGlob = require('gulp-sass-glob');
+const autoprefixer = require('gulp-autoprefixer');
+const gcmq = require('gulp-group-css-media-queries');
+const cleanCSS = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const svgo = require('gulp-svgo');
+const svgSprite = require('gulp-svg-sprite');
+const gulpif = require('gulp-if');
